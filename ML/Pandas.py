5ed9389876f061ea5fe0c636711afceb4aca75c7
MobeenButt <mobeen914butt@gmail.com>
2025-06-16 01:57:54 +0500

    Basics of Pandas

--- ML/Pandas.py
+++ ML/Pandas.py
@@ -8,3 +8,15 @@ arr=np.array(my_data)
 print(labels)
 print(my_data)
 print(arr)
+
+# syntax Series(list)
+pandas=pd.Series(data=my_data)
+print(pandas)
+
+# syntax Series(list=,index=)
+pandas1=pd.Series(data=my_data,index=labels)
+pandas1=pd.Series(my_data,labels)
+print(pandas1)
+# both are same
+pandas1=pd.Series(my_data,labels)
+print(pandas1)
