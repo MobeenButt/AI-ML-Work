4dc83854e1b2a1c908bc99da96609d4fcc14f28a
MobeenButt <mobeen914butt@gmail.com>
2025-06-16 02:01:34 +0500

    Basics of Pandas

--- ML/Pandas.py
+++ ML/Pandas.py
@@ -4,7 +4,7 @@ import numpy as np
 labels=['a','b','c','d']
 my_data=[10,20,30,40]
 arr=np.array(my_data)
-
+d={'a':10,'b':20,'c':30,'d':40}
 print(labels)
 print(my_data)
 print(arr)
@@ -20,3 +20,12 @@ print(pandas1)
 # both are same
 pandas1=pd.Series(my_data,labels)
 print(pandas1)
+
+# syntax Series(numpyArryas)
+pandas3=pd.Series(arr,labels)
+print(pandas3)
+
+# syntax Series(dictionary)
+pandas4=pd.Series(d,labels)
+print(pandas4)
+print(d)
