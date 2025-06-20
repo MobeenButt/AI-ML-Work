92eaea030a1123ba5b3c34d3834670b8cd6686e7
MobeenButt <mobeen914butt@gmail.com>
2025-06-21 02:59:27 +0500

    DataFrames in Pandas

--- ML/Pandas/DataFrames.py
+++ ML/Pandas/DataFrames.py
@@ -7,3 +7,14 @@ lis=[1,2,3,4,5,6]
 var=pd.DataFrame(lis)
 print(var)
 print(type(var))
+
+
+dic={"a":[1,2,"s",4,5],"s":[1,2,"s",4,5],"d":[1,2,"a",4,5],1:[1,2,3,"h",5]}
+var1=pd.DataFrame(dic,columns=['a',1,"d"],index=['a','b','c','d','e'])
+print(var1)
+print(var1["a"][1])  # Accessing multiple columns
+print(type(var1))
+
+# In Dictionary if values are of different lengths, it will give error
+
+# a , s are column names
